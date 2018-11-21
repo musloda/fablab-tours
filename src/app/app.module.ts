5a293e0b4bd74952043cdf2b033c9b1e48cd80ea
Valentin Berruer <valb37@icloud.com>
2018-11-21 17:49:28 +0100

    modif header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,6 +15,7 @@ import { HeaderComponent } from './header/header.component';
   imports: [
     BrowserModule,
     NgbModule,
+
 
   ],
   providers: [],
