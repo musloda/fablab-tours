766de1c40adc6fbd7ce423fcd6e9963add895ee1
Valentin Berruer <valb37@icloud.com>
2018-11-21 15:41:29 +0100

    création header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Route } from '@angular/router';
+import { HeaderComponent } from './header/header.component'
+
+
+const ROUTES: Route[] = [
+{
+	path: 'accueil',
+	component: HeaderComponent,
+},
+{ path: '',
+    redirectTo: '/accueil',
+    pathMatch: 'full' },
+
+];
+
+export { ROUTES };
