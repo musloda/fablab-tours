54d235f2832e44066d3974aeec7d4352f608b15e
Valentin Berruer <valb37@icloud.com>
2018-11-26 15:37:19 +0100

    modif navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -17,22 +17,17 @@ export class HeaderComponent implements OnInit {
 		const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 		if (number > 820) {
 			return true;
-
 		} else if (number > 2000) {
 			return false;
-
 		}
-
 	}
 
 	onWindowfix(){
 		const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 		if (number < 821) {
 			return true;
-
 		} else if (number < 2000) {
 			return false;
-
 		}
 	}
 
